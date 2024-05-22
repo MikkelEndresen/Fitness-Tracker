980736d0f251cc7d4c94846ca09af1e21984240a
Mickey <72854733+MikkelEndresen@users.noreply.github.com>
2024-05-22 20:41:17 +1000

    Added files

---
+++ src/agent.py
@@ -0,0 +1,10 @@
+
+
+
+# Workflow suggestion
+    # Does it contain information about what the bro just did?
+        # If yes
+            # Extract information and place in db
+            # chat back
+        # If no
+            # chat back
